6c2cd4dc2e817e720c6a1892acb03d5a5d3463b2
Josh Chang <joshchang04@gmail.com>
2018-12-08 12:59:27 -0500

    Update index.js

--- material/js/index.js
+++ material/js/index.js
@@ -1,5 +1,9 @@
-
 var nodes = document.querySelectorAll('.mdc-icon-toggle');
 for (var i = 0, node; node = nodes[i]; i++) {
-  mdc.iconToggle.MDCIconToggle.attachTo(node);
+  	mdc.iconToggle.MDCIconToggle.attachTo(node);
+}
+
+var bars = document.querySelectorAll('.mdc-top-app-bar');
+for (var i = 0, bar; bar = bars[i]; i++) {
+		mdc.topAppBar.MDCTopAppBar.attachTo(bar);
 }
