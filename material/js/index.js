8f2a656e1a066879f16f402dc02c19380139dfc5
Josh Chang <joshchang04@gmail.com>
2018-12-09 18:25:58 -0500

    Update index.js

--- material/js/index.js
+++ material/js/index.js
@@ -1,3 +1,19 @@
+function warningOpen() {
+	document.getElementById("alert-dialog").className = "mdc-dialog mdc-dialog--open";
+}
+function warningClosing() {
+	document.getElementById("alert-dialog").className = "mdc-dialog mdc-dialog--closing";
+	setTimeout(warningClosed, 300);
+}
+function warningClosed() {
+	document.getElementById("alert-dialog").className = "mdc-dialog mdc-dialog--closed";
+}
+
+var dialogs = document.querySelectorAll('.mdc-dialog');
+for (var i = 0, dialog; dialog = dialogs[i]; i++) {
+  	mdc.dialog.MDCDialog.attachTo(dialog);
+}
+
 var nodes = document.querySelectorAll('.mdc-icon-toggle');
 for (var i = 0, node; node = nodes[i]; i++) {
   	mdc.iconToggle.MDCIconToggle.attachTo(node);
