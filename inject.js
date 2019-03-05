bca4c4736d11115a5d775804b802f2ce02cc6cc5
Josh Chang <joshchang04@gmail.com>
2019-03-04 22:45:36 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,23 +1,25 @@
-if(window.location.href == 'https://poll.fm/10255595'){
-        var cookies = document.cookie.split(";");
-        for (var i = 0; i < cookies.length; i++)
-        {   
-            var spcook =  cookies[i].split("=");
-            deleteCookie(spcook[0]);
+document.addEventListener('DOMContentLoaded', function() {   
+        if(window.location.href == 'https://poll.fm/10255595'){
+                var cookies = document.cookie.split(";");
+                for (var i = 0; i < cookies.length; i++)
+                {   
+                    var spcook =  cookies[i].split("=");
+                    deleteCookie(spcook[0]);
+                }
+                function deleteCookie(cookiename)
+                {
+                    var d = new Date();
+                    d.setDate(d.getDate() - 1);
+                    var expires = ";expires="+d;
+                    var name=cookiename;
+                    //alert(name);
+                    var value="";
+                    document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
+                }
+                document.getElementById("PDI_answer47163907").click();
+                document.getElementsByClassName("vote-button")[0].click();
+        } 
+        if(window.location.href == 'https://poll.fm/10255595/results?msg=voted'){
+            window.location = "https://poll.fm/10255595";
         }
-        function deleteCookie(cookiename)
-        {
-            var d = new Date();
-            d.setDate(d.getDate() - 1);
-            var expires = ";expires="+d;
-            var name=cookiename;
-            //alert(name);
-            var value="";
-            document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
-        }
-        document.getElementById("PDI_answer47163907").click();
-        document.getElementsByClassName("vote-button")[0].click();
-} 
-if(window.location.href == 'https://poll.fm/10255595/results?msg=voted'){
-    window.location = "https://poll.fm/10255595";
-}
+}, false);
