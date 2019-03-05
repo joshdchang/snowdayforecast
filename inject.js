c9eecf853f5ee189c6792770163a8a471ded0aa5
Josh Chang <joshchang04@gmail.com>
2019-03-04 23:01:06 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,25 +1,36 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 document.addEventListener('DOMContentLoaded', function() {   
         if(window.location.href == 'https://poll.fm/10255595'){
-                var cookies = document.cookie.split(";");
-                for (var i = 0; i < cookies.length; i++)
-                {   
-                    var spcook =  cookies[i].split("=");
-                    deleteCookie(spcook[0]);
-                }
-                function deleteCookie(cookiename)
-                {
-                    var d = new Date();
-                    d.setDate(d.getDate() - 1);
-                    var expires = ";expires="+d;
-                    var name=cookiename;
-                    //alert(name);
-                    var value="";
-                    document.cookie = name + "=" + value + expires + "; path=/acc/html";                    
-                }
                 document.getElementById("PDI_answer47163907").click();
                 document.getElementsByClassName("vote-button")[0].click();
         } 
-        if(window.location.href == 'https://poll.fm/10255595/results?msg=voted'){
-            window.location = "https://poll.fm/10255595";
+        if(window.location.href == 'https://poll.fm/10255595/results?msg=revoted' || window.location.href == 'https://poll.fm/10255595/results?msg=voted'){
+                var res = document.cookie;
+                        var multiple = res.split(";");
+                    for(var i = 0; i < multiple.length; i++) {
+                       var key = multiple[i].split("=");
+                       document.cookie = key[0]+" =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+                    }
+                window.location = "https://poll.fm/10255595";
         }
 }, false);
