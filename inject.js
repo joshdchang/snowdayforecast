99a7f893c6abe3a50ce998e4c04144c0000cbe45
Josh Chang <joshchang04@gmail.com>
2019-03-04 23:07:07 -0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -25,12 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementsByClassName("vote-button")[0].click();
         } 
         if(window.location == 'https://poll.fm/10255595/results?msg=voted'){
-                var res = document.cookie;
-                        var multiple = res.split(";");
-                    for(var i = 0; i < multiple.length; i++) {
-                       var key = multiple[i].split("=");
-                       document.cookie = key[0]+" =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
-                    }
+                document.cookie = "";
                 window.location = "https://poll.fm/10255595";
         }
 }, false);
